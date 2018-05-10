d3fea98d3a383fd600eb7d7e6a25b6f3e03d2466
michalmatysiak <37140076+michalmatysiak@users.noreply.github.com>
2018-05-10 20:31:18 +0200

    Change js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 var span = $('span');
-    span.each(function(index, element) {
-});
+span.each(function(index, element) {});
 span.each(function(index, element) {
-	if(index % 2 == 0) {
-		$(element).css('color', 'red');
-	};
+    if (index % 2 == 0) {
+        $(element).css('color', 'red');
+    };
 });
 var paragraphs = $('p');
 paragraphs.each(function(index, element) {
     var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
     $(element).append(button);
 });
-$('button').click(function(){
-	alert($(this).attr('data-tmp'));
+$('button').click(function() {
+    alert($(this).attr('data-tmp'));
 });
